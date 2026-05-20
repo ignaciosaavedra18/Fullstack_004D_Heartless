105ce64fe2f0023fa5751b9b98ee14d97f997712
Igncio <ig.saavedrap@duocuc.cl>
2026-05-20 13:07:19 -0400

    pequeñas actualizaciones

--- src/main/java/com/duoc/heartless/model/Autor.java
+++ src/main/java/com/duoc/heartless/model/Autor.java
@@ -2,13 +2,18 @@ package com.duoc.heartless.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
+
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -31,7 +36,9 @@ public class Autor {
 
     @OneToMany(mappedBy = "autor")
     private List<Libro> libros;
+
 }
+
 
 // MODELS: Son las entidades que representan tablas de la base de datos. En este caso, la clase Autor representa la tabla "autores". Cada instancia de Autor corresponde a una fila en la tabla.
 // Entidad que representa un autor en la base de datos. Contiene atributos del autor como: id nombre nacionalidad
