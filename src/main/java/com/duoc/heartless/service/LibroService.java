721970622402b6f9a3d47b854f5d598829f60711
Igncio <ig.saavedrap@duocuc.cl>
2026-05-21 17:15:43 -0400

    cambios de postman

--- src/main/java/com/duoc/heartless/service/LibroService.java
+++ src/main/java/com/duoc/heartless/service/LibroService.java
@@ -49,7 +49,7 @@ public class LibroService {
 
         libro.setNombre(libroactualizado.getNombre());
         libro.setGenero(libroactualizado.getGenero());
-         libro.setFecha_de_inicio(libroactualizado.getFecha_de_inicio());
+        libro.setFecha_de_inicio(libroactualizado.getFecha_de_inicio());
         libro.setFecha_de_termino(libroactualizado.getFecha_de_termino());
         libro.setPagina(libroactualizado.getPagina());
         libro.setSinopsis(libroactualizado.getSinopsis());
